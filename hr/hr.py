ce7f4d136f4dee9066c23c46b06060a41927220c
Daniel Császár <daniel.cs20@gmail.com>
2017-11-21 22:35:27 +0100

    Added functions to hr.py skeleton.

--- hr/hr.py
+++ hr/hr.py
@@ -1,18 +1,16 @@
-# data structure:
-# id: string
-#     Unique and random generated (at least 2 special char()expect: ';'), 2 number, 2 lower and 2 upper case letter)
-# name: string
-# birth_date: number (year)
+"""
+data structure:
+-id: string
+ Unique and random generated (at least 2 special char()expect: ';'), 2 number, 2 lower and 2 upper case letter)
+-name: string
+-birth_date: number (year)
+"""
 
-
-# importing everything you need
 import os
-# User interface module
 import ui
-# data manager module
 import data_manager
-# common module
 import common
+import datetime
 
 
 def start_module():
@@ -24,12 +22,41 @@ def start_module():
     Returns:
         None
     """
+    options = ["Show Table",
+               "Add Person",
+               "Remove Person",
+               "Update Person",
+               "Show Oldest Person",
+               "Show Persons Closest to Average Age"]
 
-    # your cool code
+    hr_data = data_manager.get_table_from_file("persons.csv")
+    for person in hr_data:
+        person[2] = int(person[2])
 
-    print("Printing is illegal here!")
+    while True:
+        ui.print_menu("HR Department: Main menu", options, "Exit program")
+        inputs = ui.get_inputs(["Please enter a number: "], "")
+        option = inputs[0]
 
-    pass
+        if option == "1":
+            show_table(hr_data)
+        elif option == "2":
+            hr_data = add(hr_data)
+        elif option == "3":
+            to_remove = ui.get_inputs(["Please enter the ID of the person you want removed: "], "")
+            hr_data = remove(hr_data, to_remove[0])
+        elif option == "4":
+            to_update = ui.get_inputs(["Please enter the ID of the person you want updated: "], "")
+            hr_data = update(hr_data, to_update[0])
+        elif option == "5":
+            ui.print_result(get_oldest_person(hr_data))
+        elif option == "6":
+            ui.print_result(get_persons_closest_to_average(hr_data))
+        elif option == "0":
+            break
+        else:
+            ui.print_error_message(err)
+            # Wait here
 
 
 def show_table(table):
@@ -42,10 +69,7 @@ def show_table(table):
     Returns:
         None
     """
-
-    # your code
-
-    pass
+    ui.print_table(table)
 
 
 def add(table):
@@ -58,9 +82,15 @@ def add(table):
     Returns:
         Table with a new record
     """
+    new_person = [common.generate_random(table)]
+    new_person.append(ui.get_inputs(["Name: "], "New Person's Information"))
+    while True:
+        b_year = ui.get_inputs(["Birth Year: "], "")
+        if common.validate_year(b_year):
+            new_person.append(b_year)
+            break
 
-    # your code
-
+    table.append(new_person)
     return table
 
 
@@ -75,9 +105,12 @@ def remove(table, id_):
     Returns:
         Table without specified record.
     """
+    index = common.index_of_id(table, id_)
+    if index == -1:
+        ui.print_error_message("Wrong ID!")
+        return
 
-    # your code
-
+    del table[common.index_of_id(table, id_)]
     return table
 
 
@@ -92,8 +125,17 @@ def update(table, id_):
     Returns:
         table with updated record
     """
+    index = common.index_of_id(table, id_)
+    if index == -1:
+        ui.print_error_message("Wrong ID!")
+        return
 
-    # your code
+    table[index][1](ui.get_inputs(["Name: "], ""))
+    while True:
+        b_year = ui.get_inputs(["Birth Year: "], "")
+        if common.validate_year(b_year):
+            table[index][2] = b_year
+            break
 
     return table
 
@@ -104,16 +146,13 @@ def update(table, id_):
 # the question: Who is the oldest person ?
 # return type: list of strings (name or names if there are two more with the same value)
 def get_oldest_person(table):
-
-    # your code
-
-    pass
+    max_age = min([person[2] for person in table])
+    return [person[1] for person in table if person[2] == max_age]
 
 
 # the question: Who is the closest to the average age ?
 # return type: list of strings (name or names if there are two more with the same value)
 def get_persons_closest_to_average(table):
-
-    # your code
-
-    pass
+    average_age = common.get_sum(table, 2) // common.count(table)
+    closest_age = min([abs(average_age-person[2]) for person in table])
+    return [person[1] for person in table if abs(average_age-person[2]) == closest_age]
