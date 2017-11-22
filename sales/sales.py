c90ec87e21a37e1d4ed4fc080f2b952227ecf6ff
Daniel Császár <daniel.cs20@gmail.com>
2017-11-22 21:30:23 +0100

    Added graceful dgr. Fixed type casting. Fixed get_lowest price to actually show lowest not highest price item. Note: may need to still change the ordering.

--- sales/sales.py
+++ sales/sales.py
@@ -34,22 +34,22 @@ def start_module():
     """
 
     options = ["Show Table",
-               "Add Person",
-               "Remove Person",
-               "Update Person",
+               "Add Sale",
+               "Remove Sale",
+               "Update Sale",
                "Show Lowest Price ID",
                "Show Sold Items Between Dates"]
 
     sales_data = data_manager.get_table_from_file("sales/sales.csv")
-    for sale in sales_data:
-        sale[PRICE] = int(sale[PRICE])
-        sale[MONTH] = int(sale[MONTH])
-        sale[DAY] = int(sale[DAY])
-        sale[YEAR] = int(sale[YEAR])
 
     while True:
         ui.print_menu("Sales Department: Main menu", options, "Exit program")
-        inputs = ui.get_inputs(["Please enter a number: "], "")
+        try:
+            inputs = ui.get_inputs(["Please enter a number: "], "")
+        except (KeyboardInterrupt, EOFError):
+            data_manager.write_table_to_file("sales/sales.csv", sales_data)
+            common.exit()
+
         option = inputs[0]
 
         if option == "1":
@@ -67,11 +67,6 @@ def start_module():
         elif option == "6":
             ui.print_table(get_items_sold_between(sales_data, 12, 4, 2015, 2, 7, 2016), ["1", "2", "3", "4", "5", "6"])
         elif option == "0":
-            for sale in sales_data:
-                sale[PRICE] = str(sale[PRICE])
-                sale[MONTH] = str(sale[MONTH])
-                sale[DAY] = str(sale[DAY])
-                sale[YEAR] = str(sale[YEAR])
             data_manager.write_table_to_file("sales/sales.csv", sales_data)
             break
         else:
@@ -90,7 +85,7 @@ def show_table(table):
     """
     titles = ["ID", "Title", "Price", "Date"]
     output_table = [[row[ID], row[TITLE], row[PRICE],
-                     '/'.join((str(row[YEAR]), str(row[MONTH]), str(row[DAY])))] for row in table]
+                     '/'.join((row[YEAR], row[MONTH], row[DAY]))] for row in table]
     ui.print_table(output_table, titles)
 
 
@@ -110,25 +105,25 @@ def add(table):
     while True:
         price = ui.get_inputs(["Price: "], "")[0]
         if common.validate_int(price):
-            new_sale.append(int(price))
-            break
-
-    while True:
-        year = ui.get_inputs(["Year of sale: "], "")[0]
-        if common.validate_byear(year):
-            new_sale.append(int(year))
+            new_sale.append(price)
             break
 
     while True:
         month = ui.get_inputs(["Month of sale: "], "")[0]
         if common.validate_month(month):
-            new_sale.append(int(month))
+            new_sale.append(month)
             break
 
     while True:
         day = ui.get_inputs(["Day of sale: "], "")[0]
         if common.validate_day(day):
-            new_sale.append(int(day))
+            new_sale.append(day)
+            break
+
+    while True:
+        year = ui.get_inputs(["Year of sale: "], "")[0]
+        if common.validate_byear(year):
+            new_sale.append(year)
             break
 
     table.append(new_sale)
@@ -176,25 +171,25 @@ def update(table, id_):
     while True:
         price = ui.get_inputs(["Price: "], "")[0]
         if common.validate_int(price):
-            table[index][PRICE] = int(price)
+            table[index][PRICE] = price
             break
 
     while True:
         year = ui.get_inputs(["Year of sale: "], "")[0]
         if common.validate_byear(year):
-            table[index][YEAR] = int(year)
+            table[index][YEAR] = year
             break
 
     while True:
         month = ui.get_inputs(["Month of sale: "], "")[0]
         if common.validate_month(month):
-            table[index][MONTH] = int(month)
+            table[index][MONTH] = month
             break
 
     while True:
         day = ui.get_inputs(["Day of sale: "], "")[0]
         if common.validate_day(day):
-            table[index][DAY] = int(day)
+            table[index][DAY] = day
             break
 
     return table
@@ -208,18 +203,18 @@ def update(table, id_):
 # if there are more than one with the lowest price, return the first by descending alphabetical order
 def get_lowest_price_item_id(table):
 
-    prices = [(line[ID], line[TITLE], line[PRICE]) for line in table]
-    max_price = max(prices, key=itemgetter(1))[1]
-    max_price_items = [(item[ID], item[TITLE]) for item in prices if item[1] == max_price]
-
-    return common.qsort(max_price_items, key=itemgetter(1), reversed=True)[0][ID]
+    prices = [(line[ID], line[TITLE], int(line[PRICE])) for line in table]
+    min_price = min(prices, key=itemgetter(2))[2]
+    min_price_items = [(item[ID], item[TITLE], item[PRICE]) for item in prices if item[2] == min_price]
+    # IMPORTANT NOTE: Assignment not clear on whether we should take the alpha-order of IDs or titles.
+    return common.qsort(min_price_items, key=itemgetter(1), reversed=True)[0][ID]
 
 
 # the question: Which items are sold between two given dates ? (from_date < sale_date < to_date)
 # return type: list of lists (the filtered table)
 def get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to):
 
-    min_date = datetime(year_from, month_from, day_from)
-    max_date = datetime(year_to, month_to, day_to)
+    min_date = datetime(int(year_from), int(month_from), int(day_from))
+    max_date = datetime(int(year_to), int(month_to), int(day_to))
 
-    return [line for line in table if min_date < datetime(line[YEAR], line[MONTH], line[DAY]) < max_date]
+    return [line for line in table if min_date < datetime(int(line[YEAR]), int(line[MONTH]), int(line[DAY])) < max_date]
