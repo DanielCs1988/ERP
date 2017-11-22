6352aa5a8e2dcdc390eead50cb4041074a690ca5
Leblayd <leblaydavid7@gmail.com>
2017-11-22 11:57:04 +0100

    Created validate_int and modified existing validations

--- common.py
+++ common.py
@@ -159,13 +159,9 @@ def generate_random(table):
 def validate_byear(year):
     "Check if parameter is an integer and whether it's less or equal than the current year."
 
-    try:
-        year = int(year)
-    except ValueError:
-        return False
-    else:
-        if year > datetime.now().year:
-            return False
+    if validate_int(month):
+        month = int(month)
+    elif year > datetime.now().year:
         return True
 
 
@@ -188,7 +184,9 @@ def validate_boolean(boolean):
 def validate_month(month):
     """Check if parameter is a valid month by number (1-12), returns false otherwise"""
 
-    if month not in tuple(range(1, 13)):
+    if validate_int(month):
+        month = int(month)
+    elif month not in tuple(range(1, 13)):
         return False
     return True
 
@@ -199,7 +197,9 @@ def validate_day(day):
     Does not differentiate between months, so february 31 is possible.
     """
 
-    if month not in tuple(range(1, 31)):
+    if validate_int(month):
+        month = int(month)
+    elif month not in tuple(range(1, 32)):
         return False
     return True
 
@@ -216,3 +216,14 @@ def validate_email(email):
         True, if email is a valid email address
     '''
     return re.match(r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$", email) is not None
+
+
+def validate_int(integer):
+    '''
+    Checks if the parameter is a valid integer, returns false otherwise.
+    '''
+    try:
+        integer = int(integer)
+    except ValueError:
+        return False
+    return True
