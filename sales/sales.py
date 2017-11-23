852914f1eb57581b23b6ffe25ff58d23d36cfe6d
Daniel Császár <daniel.cs20@gmail.com>
2017-11-23 13:30:22 +0100

    Removed some invalid imports, implemented new common functions for add, update and remove.

--- sales/sales.py
+++ sales/sales.py
@@ -8,8 +8,6 @@
 # year: number
 # month,year and day combined gives the date the sale was made
 
-from datetime import datetime
-from operator import itemgetter
 import os
 import ui
 import data_manager
@@ -41,39 +39,45 @@ def start_module():
                "Show Sold Items Between Dates"]
 
     sales_data = data_manager.get_table_from_file("sales/sales.csv")
+    ui.clear_scr()
 
     while True:
         ui.print_menu("Sales Department: Main menu", options, "Exit program")
-        try:
-            inputs = ui.get_inputs(["Please enter a number: "], "")
-        except (KeyboardInterrupt, EOFError):
-            data_manager.write_table_to_file("sales/sales.csv", sales_data)
-            common.exit()
-
-        option = inputs[0]
+        option = ui.getch()
 
         if option == "1":
             show_table(sales_data)
         elif option == "2":
             sales_data = add(sales_data)
+            ui.clear_scr()
         elif option == "3":
-            to_remove = ui.get_inputs(["Please enter the ID of the sale you want removed: "], "")
-            sales_data = remove(sales_data, to_remove[0])
+            to_remove = ui.valid_input(
+                "What is the ID of the item that you would like to remove? ", common.validate_string)
+            sales_data = remove(sales_data, to_remove)
+            ui.clear_scr()
         elif option == "4":
-            to_update = ui.get_inputs(["Please enter the ID of the sale you want updated: "], "")
-            sales_data = update(sales_data, to_update[0])
+            to_update = ui.valid_input(
+                "What is the ID of the item that you would like to update? ", common.validate_string)
+            sales_data = update(sales_data, to_update)
+            ui.clear_scr()
         elif option == "5":
             ui.print_result(get_lowest_price_item_id(sales_data))
         elif option == "6":
-            params = ui.mass_valid_input([
-                
-            ])
-            ui.print_table(get_items_sold_between(sales_data, 12, 4, 2015, 2, 7, 2016), ["1", "2", "3", "4", "5", "6"])
+            params = ui.mass_valid_input([("Month from:", common.validate_month),
+                                          ("Day from: ", common.validate_day),
+                                          ("Year from: ", common.validate_byear),
+                                          ("Month to:", common.validate_month),
+                                          ("Day to: ", common.validate_day),
+                                          ("Year to: ", common.validate_byear)
+                                          ])
+            if params:
+                show_table(get_items_sold_between(sales_data, *params))
         elif option == "0":
             data_manager.write_table_to_file("sales/sales.csv", sales_data)
+            ui.clear_scr()
             break
         else:
-            ui.print_error_message(err)
+            ui.clear_scr()
 
 
 def show_table(table):
@@ -89,6 +93,7 @@ def show_table(table):
     titles = ["ID", "Title", "Price", "Date"]
     output_table = [[row[ID], row[TITLE], row[PRICE],
                      '/'.join((row[YEAR], row[MONTH], row[DAY]))] for row in table]
+    ui.clear_scr()
     ui.print_table(output_table, titles)
 
 
@@ -104,7 +109,7 @@ def add(table):
     """
     new_sale = [common.generate_random(table)]
 
-    input_list = ui.mass_valid_input([("Title: ", None),
+    input_list = ui.mass_valid_input([("Title: ", common.validate_string),
                                       ("Price: ", common.validate_int),
                                       ("Month of sale: ", common.validate_month),
                                       ("Day of sale: ", common.validate_day),
@@ -157,13 +162,7 @@ def update(table, id_):
                                       ("Year of sale: ", common.validate_byear)
                                       ], update_mode=True)
 
-    if input_list is None:
-        return table
-
-    for new_item in range(len(input_list)):
-        if input_list[new_item]:
-            table[index][new_item+1] = input_list[new_item]
-
+    table[index] = common.apply_update_to_line(table[index], input_list)
     return table
 
 
@@ -176,17 +175,17 @@ def update(table, id_):
 def get_lowest_price_item_id(table):
 
     prices = [(line[ID], line[TITLE], int(line[PRICE])) for line in table]
-    min_price = min(prices, key=itemgetter(2))[2]
+    min_price = min(prices, key=common.get_item(2))[2]
     min_price_items = [(item[ID], item[TITLE], item[PRICE]) for item in prices if item[2] == min_price]
     # IMPORTANT NOTE: Assignment not clear on whether we should take the alpha-order of IDs or titles.
-    return common.qsort(min_price_items, key=itemgetter(1), reversed=True)[0][ID]
+    return common.qsort(min_price_items, key=common.get_item0(1), reversed=True)[0][ID]
 
 
 # the question: Which items are sold between two given dates ? (from_date < sale_date < to_date)
 # return type: list of lists (the filtered table)
 def get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to):
 
-    min_date = datetime(int(year_from), int(month_from), int(day_from))
-    max_date = datetime(int(year_to), int(month_to), int(day_to))
+    min_date = common.dtime(year_from, month_from, day_from)
+    max_date = common.dtime(year_to, month_to, day_to)
 
-    return [line for line in table if min_date < datetime(int(line[YEAR]), int(line[MONTH]), int(line[DAY])) < max_date]
+    return [line for line in table if min_date < common.dtime(line[YEAR], line[MONTH], line[DAY]) < max_date]
