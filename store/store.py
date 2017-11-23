cd149a777089841406ae82cf294fb08c896bb902
Balázs Vajner <vajner.balazs@gmail.com>
2017-11-23 15:52:20 +0100

    Store module bugfix

--- store/store.py
+++ store/store.py
@@ -40,44 +40,47 @@ def start_module():
                "Show Average Game Count of a Manufacturer"]
 
     store_data = data_manager.get_table_from_file("store/games.csv")
-    for game in store_data:
-        game[PRICE] = int(game[PRICE])
-        game[IN_STOCK] = int(game[IN_STOCK])
 
-    while True:
-        ui.print_menu("Store: Main menu", options, "Exit program")
-        option = ui.getch()
+    try:
+        while True:
+            ui.print_menu("Store: Main menu", options, "Exit program")
+            option = ui.get_inputs(["Please enter a number: "], "")[0]
 
-        if option == "1":
-            show_table(store_data)
-        elif option == "2":
-            store_data = add(store_data)
-            ui.clear_scr()
-        elif option == "3":
-            to_update = ui.get_inputs(["Please enter the ID of the game you want updated: "], "")
-            store_data = update(store_data, to_update[0])
-            ui.clear_scr()
-        elif option == "4":
-            to_remove = ui.get_inputs(["Please enter the ID of the game you want removed: "], "")
-            store_data = remove(store_data, to_remove[0])
-            ui.clear_scr()
-        elif option == "5":
-            count_by_manufacturer_dict = get_counts_by_manufacturers(store_data)
-            count_by_manufacturer_table = [(manufacturer, num)
-                                           for manufacturer, num in count_by_manufacturer_dict.items()]
-            ui.print_table(count_by_manufacturer_table, ["Manufacturer", "Count"])
-        elif option == "6":
-            manufacturer = ui.get_inputs(["Please a manufacturer: "], "")[0]
-            ui.print_result(get_average_by_manufacturer(store_data, manufacturer))
-        elif option == "0":
-            for game in store_data:
-                game[PRICE] = str(game[PRICE])
-                game[IN_STOCK] = str(game[IN_STOCK])
-            data_manager.write_table_to_file("store/games.csv", store_data)
-            ui.clear_scr()
-            break
-        else:
-            ui.clear_scr()
+            if option == "1":
+                show_table(store_data)
+            elif option == "2":
+                store_data = add(store_data)
+                ui.clear_scr()
+            elif option == "3":
+                to_update = ui.get_inputs(["Please enter the ID of the game you want updated: "], "")
+                store_data = update(store_data, to_update[0])
+                ui.clear_scr()
+            elif option == "4":
+                to_remove = ui.get_inputs(["Please enter the ID of the game you want removed: "], "")
+                store_data = remove(store_data, to_remove[0])
+                ui.clear_scr()
+            elif option == "5":
+                count_by_manufacturer_dict = get_counts_by_manufacturers(store_data)
+                count_by_manufacturer_table = [(manufacturer, num)
+                                            for manufacturer, num in count_by_manufacturer_dict.items()]
+                ui.print_table(count_by_manufacturer_table, ["Manufacturer", "Count"])
+            elif option == "6":
+                manufacturer = ui.get_inputs(["Please a manufacturer: "], "")[0]
+                ui.print_result(get_average_by_manufacturer(store_data, manufacturer))
+            elif option == "0":
+                for game in store_data:
+                    game[PRICE] = str(game[PRICE])
+                    game[IN_STOCK] = str(game[IN_STOCK])
+                data_manager.write_table_to_file("store/games.csv", store_data)
+                ui.clear_scr()
+                break
+            else:
+                ui.clear_scr()
+    except (KeyboardInterrupt, EOFError):
+        ui.clear_scr()
+        data_manager.write_table_to_file("store/games.csv", store_data)
+        ui.print_error_message("Keyboard interrupt. If you want to got back to main menu, use the menu.")
+        exit()
 
 
 def show_table(table):
@@ -105,16 +108,17 @@ def add(table):
         Table with a new record
     """
 
-    new_store = [common.generate_random(table)]
+    new_item = [common.generate_random(table)]
 
-    new_store.extend(ui.mass_valid_in([("Title: ", common.validate_string),
-                                       ("Manufacturer: ", common.validate_string)
-                                       ("Price: ", common.validate_int),
-                                       ("In Stock: ", common.validate_int)]))
-    if new_store is None:
+    user_input = ui.mass_valid_in([("Title: ", common.validate_string),
+                                   ("Manufacturer: ", common.validate_string),
+                                   ("Price: ", common.validate_int),
+                                   ("In Stock: ", common.validate_int)])
+    if user_input is None:
         return table
 
-    table.append(new_store)
+    new_item.extend(user_input)
+    table.append(new_item)
 
     return table
 
@@ -151,9 +155,9 @@ def update(table, id_):
         return table
 
     update_input = ui.mass_valid_in([("Title: ", common.validate_string),
-                                     ("Manufacturer: ", common.validate_string)
+                                     ("Manufacturer: ", common.validate_string),
                                      ("Price: ", common.validate_int),
-                                     ("In Stock: ", common.validate_int)])
+                                     ("In Stock: ", common.validate_int)], True)
 
     table[index] = common.apply_update_to_line(table[index], update_input)
 
