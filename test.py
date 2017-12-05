859eefb64ea893104c2eced9d4cfa9c57fbedf05
Balázs Vajner <vajner.balazs@gmail.com>
2017-12-05 11:40:56 +0100

    Added test_get_all_sales_ids_for_customer_ids_form_table to SalesTester.

--- test.py
+++ test.py
@@ -202,6 +202,16 @@ class SalesTester(unittest.TestCase):
         result = sales.get_all_customer_ids_from_table(table)
         self.assertSetEqual(expected, result)
 
+    def test_get_all_sales_ids_for_customer_ids_form_table(self):
+        table = data_manager.get_table_from_file(self.data_file)
+        expected = {"jH34Jk#&":
+                    ["kH34Ju#&", "jH34Ju#&", "tH34Ju#&", "eH34Ju#&", "kH14Ju#&", "kH35Ju#&", "kH38Ju#&", "kH94Ju#&", "tH34Jl#&", "eH34Jy#&", "bH34Jx#&"],
+                    "kH14Jt#&":
+                    ["bH34Ju#&", "vH34Ju#&", "kH34Ji#&", "vH34Jz#&", "kH14Jt#&", "kH35Jr#&", "kH38Je#&", "kH94Jw#&"],
+                    "kH14Jh#&":
+                    ["jH34Jk#&"]}
+        result = sales.get_all_sales_ids_for_customer_ids_form_table(table)
+        self.assertDictEqual(expected, result)
 
 class StoreTester(unittest.TestCase):
     data_file = "store/games_test.csv"
