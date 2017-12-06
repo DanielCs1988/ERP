ba470e17a55c67c835bb9c4f457730fe8952b086
Daniel Császár <daniel.cs20@gmail.com>
2017-12-06 14:40:59 +0100

    Implemented get buyer emails function and corresponding test.

--- common.py
+++ common.py
@@ -400,7 +400,7 @@ def trial_version(menu_options, characters):
         menu_options.append("Buy the full version of the software to unlock more options")
 
 
-def handle_kb_interrupt(filename, table_to_save):
+def handle_kb_interrupt(filename=None, table_to_save=None):
     """
     Handles keyboard interrupt exceptions. Display a message to the user, save the current module's data \
         and exits the program.
@@ -413,7 +413,8 @@ def handle_kb_interrupt(filename, table_to_save):
         None
     """
     ui.clear_scr()
-    data_manager.write_table_to_file(filename, table_to_save)
+    if filename and table_to_save:
+        data_manager.write_table_to_file(filename, table_to_save)
     ui.print_error_message("Keyboard interrupt. If you want to got back to main menu, use the menu.")
     exit()
 
